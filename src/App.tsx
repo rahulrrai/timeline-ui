0353eac8a5305888603843d4a8f79d3b6156b92b
Deepak Soni <deepak@mavericks-consulting.com>
2021-02-05 22:10:00 +0530

    sample changes and added gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rahul Rai
-          Edit <code>src/App.tsx</code> and save to reload.
+          Rahul Rai Edit <code>src/App.tsx</code> and save to reload. Awesome
+          Man!
         </p>
         <a
           className="App-link"
